818135837094be270d864a6aebf1909f41f64d24
Yannick Gerber <varmox@protonmail.ch>
2024-10-08 21:55:22 +0200

    Create _template.ps1

---
+++ _template.ps1
@@ -0,0 +1,30 @@
+<#
+.SYNOPSIS
+    
+.DESCRIPTION
+   
+.OUTPUTS
+    Results are printed to the console.
+.NOTES
+  
+    Author        Yannick Gerber, yannick.gerber@soultec.ch
+
+
+#>
+
+# Load the PowerCLI SnapIn and set the configuration
+Add-PSSnapin VMware.VimAutomation.Core -ea "SilentlyContinue"
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
+
+# Get the vCenter Server address, username and password as PSCredential
+$vCenterServer = Read-Host "Enter vCenter Server host name (DNS with FQDN or IP address)"
+$vCenterUser = Read-Host "Enter your user name (DOMAIN\User or user@domain.com)"
+$vCenterUserPassword = Read-Host "Enter your password (this will be converted to a secure string)" -AsSecureString:$true
+$Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $vCenterUser,$vCenterUserPassword
+
+# Connect to the vCenter Server with collected credentials
+Connect-VIServer -Server $vCenterServer -Credential $Credentials | Out-Null
+Write-Host "Connected to your vCenter server $vCenterServer" -ForegroundColor Green
+
+Add-PSSnapin VMware.VimAutomation.Core -ea "SilentlyContinue"
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
